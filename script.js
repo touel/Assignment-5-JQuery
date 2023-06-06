2cef091b264283bd7257cabb82b423c6bf40326f
Travis Ouellette <travisrouellette@gmail.com>
2023-06-06 09:50:17 -0400

    Render

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 var today = dayjs();
 $('#currentDay').text(today.format('MMM D, YYYY'));
+var time = new Date();
+var hours = time.getHours();
+var saveButton = document.getElementById("save");
+var task = document.querySelector("#description");
 
-$(function () {
-});
+
+function renderLastTasks() {
+};
+
+saveButton.addEventListener("click", function(event){
+    event.preventDefault();
+
+
+})
